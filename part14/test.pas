b76ae2fc8e6eb098acba5463dbbf1f968b604166
Onion-Shen <18513385631@163.com>
2020-06-27 13:54:41 +0800

    part14 commit

---
+++ part14/test.pas
@@ -0,0 +1,18 @@
+PROGRAM Main;
+   var x, y : real;
+
+   procedure AlphaA(a : integer);
+      var y : integer;
+   BEGIN { AlphaA }
+
+   END;  { AlphaA }
+
+   procedure AlphaB(a : integer);
+      var b : integer;
+   BEGIN { AlphaB }
+
+   END;  { AlphaB }
+
+BEGIN { Main }
+
+END.  { Main }
